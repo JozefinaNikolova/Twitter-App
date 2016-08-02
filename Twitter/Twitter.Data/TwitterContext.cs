9ccd4d2ff440a15c08419051c255af881a8fe479
Jozefina <jozi5@abv.bg>
2016-08-02 21:34:32 +0300

    Added data, models and web projects

---
+++ Twitter/Twitter.Data/TwitterContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Twitter.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Twitter.Models;
+
+    public class TwitterContext : IdentityDbContext<User>
+    {
+        public TwitterContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TwitterContext Create()
+        {
+            return new TwitterContext();
+        }
+    }
+}
